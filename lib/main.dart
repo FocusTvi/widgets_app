51a0c5c293c19d0de94d17dcb1291381049500c2
Jaime <jaimicopanda@hotmail.com>
2023-09-20 16:28:36 -0400

    basic routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:widgets_app/config/theme/app_theme.dart';
+import 'package:widgets_app/presentation/screens/buttons/buttons_screen.dart';
+import 'package:widgets_app/presentation/screens/cars/cards_screen.dart';
 import 'package:widgets_app/presentation/screens/home/home_screen.dart';
 
 void main() {
@@ -13,9 +15,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: AppTheme(selectedColor:0).getTheme(),
+      theme: AppTheme(selectedColor: 0).getTheme(),
       home: const HomeScreen(),
+      routes: {
+        "/buttons": (context) => const ButtonsScreen(),
+        "/cards": (context) => const CardsScreen(),
+      },
     );
   }
 }
