407aa4e163aff3315459c40d5d1305c8ab47078b
Jaime <jaimicopanda@hotmail.com>
2023-09-22 10:37:34 -0400

    progress indicators

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -24,4 +24,9 @@ const appMenuItems = <MenuItem>[
       subTitle: "Styled Cards",
       link: "/cards",
       icon: Icons.credit_card),
+  MenuItem(
+      title: "Progress Indicators",
+      subTitle: "Styled Cards",
+      link: "/progress",
+      icon: Icons.refresh_outlined),
 ];
